577ef6227daa2206e3b45379619c4bd0110a8c93
nastya312 <kaisba00@mail.ru>
2021-02-24 23:57:34 +0300

    comm_blocks

--- travel/views.py
+++ travel/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 def home(request):
     name = 'Bob'
 
-    return render(request, 'home.html', {'name':name})
+    return render(request, 'home.html', {'name': name})
+
+
+def about(request):
+    name = 'About'
+
+    return render(request, 'about.html', {'name': name})
