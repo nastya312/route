798b5cc47490ea232dc11d2ed601935fbce177ec
nastya312 <kaisba00@mail.ru>
2021-03-01 02:18:14 +0300

    templates_id

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from cities.models import City
+
+__all__ = (
+    'home',
+)
+
+
+def home(request, pk=None):
+    if pk:
+        city = get_object_or_404(City, id=pk)
+        # city = City.object.filter(id=pk).first()
+        # city = City.object.get(id=pk)
+
+        context = {'object': city}
+        return render(request, 'cities/detail.html', context)
+    gs = City.objects.all()
+    context = {'objects_list': gs}
+    return render(request, 'cities/home.html', context)
