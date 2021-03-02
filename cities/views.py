c400bf4da516425096d4903f30ddfde3de6ad7ee
nastya312 <kaisba00@mail.ru>
2021-03-03 02:58:59 +0300

    comm_class_form_detail

--- cities/views.py
+++ cities/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 
+from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 __all__ = (
-    'home',
+    'home', 'CityDetailView'
 )
 
 
 def home(request, pk=None):
-    if pk:
-        city = get_object_or_404(City, id=pk)
-        # city = City.object.filter(id=pk).first()
-        # city = City.object.get(id=pk)
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+    # if pk:
+    # city = get_object_or_404(City, id=pk)
+    # city = City.object.filter(id=pk).first()
+    # city = City.object.get(id=pk)
+    # context = {'object': city}
+    # return render(request, 'cities/detail.html', context)
 
-        context = {'object': city}
-        return render(request, 'cities/detail.html', context)
+    form = CityForm()
     gs = City.objects.all()
-    context = {'objects_list': gs}
+    context = {'objects_list': gs, 'form': form}
     return render(request, 'cities/home.html', context)
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
