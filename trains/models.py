2a719926378c32e12bca49370f7837ff7e88ed1e
nastya312 <kaisba00@mail.ru>
2021-03-10 01:53:03 +0300

    comm_new_app

---
+++ trains/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+from cities.models import City
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=30, unique=True,
+                            verbose_name='Номер поезда')
+    travel_time = models.PositiveSmallIntegerField(verbose_name='Время в пути')
+    from_city = models.ForeignKey(City, on_delete=models.CASCADE,
+                                  # null=True, blank=True, # для models.SET_NULL
+                                  related_name='from_city_set',
+                                  verbose_name='Откуда'
+                                  )
+    to_city = models.ForeignKey('cities.City', on_delete=models.CASCADE,
+                                related_name='to_city_set',
+                                verbose_name='Куда'
+                                )
+
+    def __str__(self):
+        return f'Поезд №{self.name} из города {self.from_city}'
+
+    class Meta:
+        verbose_name = 'Поезд'
+        verbose_name_plural = 'Поезда'
+        ordering = ['travel_time']
