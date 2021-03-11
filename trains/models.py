0f36a6ba4572db598e674e9e4ed79bea7774929a
nastya312 <kaisba00@mail.ru>
2021-03-11 19:06:40 +0300

    comm_check

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
@@ -25,3 +26,18 @@ class Train(models.Model):
         verbose_name = 'Поезд'
         verbose_name_plural = 'Поезда'
         ordering = ['travel_time']
+
+    def clean(self):
+        if self.from_city == self.to_city:
+            raise ValidationError('Измените город прибытия')
+        qs = Train.objects.filter(
+            from_city=self.from_city,
+            to_city=self.to_city,
+            travel_time=self.travel_time).exclude(pk=self.id)
+        # Train == self.__class__
+        if qs.exists():
+            raise ValidationError('Измените время в пути')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(Train, self).save(*args, **kwargs)
